9e5ebf9ba62f1aa904c8302001c4207c18e19326
PKS <pkumarsaha71@gmail.com>
2019-11-09 23:22:31 +0600

    correction!

--- app/src/main/java/com/example/tictoctoeandroid/MainActivity.java
+++ app/src/main/java/com/example/tictoctoeandroid/MainActivity.java
@@ -6,14 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button[][] buttons = new Button[3][3];
     private Boolean player1Turn = true;
     private int roundCount;
-    private int Player1Point;
-    private int Player2Point;
+    private int player1Points;
+    private int player2Points;
     private TextView textViewPlayer1;
     private TextView textViewPlayer2;
     Button resetBtn;
@@ -76,14 +77,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             private void  draw()
             {
 
+                Toast.makeText(getApplicationContext(), "Draw!", Toast.LENGTH_SHORT).show();
+                resetBoard();
             }
             private void player2Wins() {
+                player2Points++;
+                Toast.makeText(getApplicationContext(), "Player 2 wins!", Toast.LENGTH_SHORT).show();
+                updatePointsText();
+                resetBoard();
             }
 
             private void player1Wins() {
+                player1Points++;
+                Toast.makeText(getApplicationContext(), "Player 1 wins!", Toast.LENGTH_SHORT).show();
+                updatePointsText();
+                resetBoard();
             }
         });
     }
+
+
+    private void updatePointsText() {
+
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard() {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
+
 
     private boolean checkForWin() {
         String[][] field = new String[3][3];
